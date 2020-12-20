6841512a180005cde202143680fe96ce6ab62e50
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-12-19 21:48:38 -0700

    add run struct

---
+++ src/run.rs
@@ -0,0 +1,10 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Run {
+	game_title: String,
+}
+
+impl Run {
+    // overarching functions will live here
+}
